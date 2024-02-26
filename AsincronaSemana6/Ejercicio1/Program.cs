e057c4a38e71e6562cc9688a3bb6c8066546ed07
RBryan05 <126432796+RBryan05@users.noreply.github.com>
2024-02-26 13:21:56 -0600

    Add files via upload

---
+++ AsincronaSemana6/Ejercicio1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Ejercicio1
+{
+    public class Program
+    {
+        static string[] nombres = { "Bryan", "Fernando", "Luz", "Marisol", "Oscar", "Marta", "Ruben"};
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nombres que contienen la letra A");
+            FuncionFiltrarNombreA();
+        }
+
+        static void FuncionFiltrarNombreA()
+        {
+
+            Console.WriteLine("Expresion LINQ\n");
+            var filtar = from nombre in nombres
+                         where nombre.Contains('a')
+                         select nombre;
+            
+            foreach (var nombre in filtar)
+            {
+                Console.WriteLine(nombre);
+            }
+
+            Console.WriteLine("\nExpresion LAMBDA\n");
+            var filtar2 = nombres.Where(x => x.Contains('a'));
+            foreach (var nombre in filtar2)
+            {
+                Console.WriteLine(nombre);
+            }
+        }
+    }
+}
---
+++ AsincronaSemana6/Ejercicio2/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Ejercicio2
+{
+    public class Program
+    {
+
+        static int[] numeros = { 8, -3, 3, 4, 90, 9, 16, 100, 1, 2, 10, 7 };
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nSuma de los numeros mayores que 5 definidos en un array.\n");
+            SumaNumerosMayores5();
+        }
+
+        static void SumaNumerosMayores5()
+        {
+
+            Console.WriteLine("Expresion LINQ\n");
+            var filtro = from numero in numeros
+                         where numero > 5
+                         select numero;
+
+            var suma = 0;
+            Console.WriteLine("Numeros mayores que 5\n");
+            foreach (int numero in filtro)
+            {
+                suma += numero;
+                Console.WriteLine(numero);
+            }
+            Console.WriteLine($"\nLa suma de los numeros mayores que 5 es: {suma}\n");
+
+            Console.WriteLine("Expresion LAMBDA\n");
+            var filtro2 = numeros.Where(y => y > 5);
+
+            var suma2 = 0;
+            Console.WriteLine("Numeros mayores que 5\n");
+            foreach (int numero in filtro2)
+            {
+                suma2 += numero;
+                Console.WriteLine(numero);
+            }
+            Console.WriteLine($"\nLa suma de los numeros mayores que 5 es: {suma2}\n");
+        }
+    }
+}
